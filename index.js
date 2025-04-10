a1aee7bda2c2d7fd5309a086b086cc0b2936c76b
CRISS VEVO <crissvevo@icloud.com>
2025-04-10 08:58:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ async function downloadSessionData() {
         return false;
     }
 
-    const sessdata = config.SESSION_ID.split("Demo-Slayer")[1];
+    const sessdata = config.SESSION_ID.split("Demo-Slayer~")[1];
 
     if (!sessdata || !sessdata.includes("#")) {
         console.error('Invalid SESSION_ID format! It must contain both file ID and decryption key.');
