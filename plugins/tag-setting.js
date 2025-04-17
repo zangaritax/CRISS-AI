63665312e483d80cafa5cb8fd4d81e7d10fec496
criss <crissvevo@icloud.com>
2025-04-17 08:29:40 +0300

    Update tag-setting.js

--- plugins/tag-setting.js
+++ plugins/tag-setting.js
@@ -53,29 +53,3 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
         reply(`❌ *Error Occurred !!*\n\n${e.message || e}`);
     }
 });
-
-cmd({
-    pattern: "tag",
-    react: "🔊",
-    desc: "To tag all members with a message",
-    category: "group",
-    use: '.tag Hi',
-    filename: __filename
-}, async (conn, mek, m, { from, senderNumber, participants, q, reply }) => {
-    try {
-        // Get the bot owner's number dynamically from conn.user.id
-        const botOwner = conn.user.id.split(":")[0]; // Extract the bot owner's number
-        if (senderNumber !== botOwner) {
-            return reply("Only the bot owner can use this command.");
-        }
-
-        if (!q) return reply('*Please provide a message to send.* ℹ️');
-
-        conn.sendMessage(from, { text: q, mentions: participants.map(a => a.id), linkPreview: true }, { quoted: mek });
-
-    } catch (e) {
-        await conn.sendMessage(from, { react: { text: '❌', key: mek.key } });
-        console.log(e);
-        reply(`❌ *Error Occurred !!*\n\n${e}`);
-    }
-});
