187e01c38d51baf312cb425bf1fd6ac4ba3c92ff
criss-vevo <crissvevo@icloud.com>
2025-05-05 13:32:19 +0300

    @criss-vevo

--- plugins/main-repo.js
+++ plugins/main-repo.js
@@ -47,7 +47,7 @@ async (conn, mek, m, { from, reply }) => {
 
         // Send the audio file with context info
         await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/XdTechPro/KHAN-DATA/raw/refs/heads/main/autovoice/menunew.m4a' },
+            audio: { url: 'https://github.com/criss-vevo/CRISS-DATA/raw/refs/heads/main/autovoice/menunew.m4a' },
             mimetype: 'audio/mp4',
             ptt: true,
             contextInfo: { 
