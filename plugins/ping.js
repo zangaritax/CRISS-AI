9b7a4f24858cd7d2f69e369b003a227f231e0bf3
criss-vevo <crissvevo@icloud.com>
2025-05-18 23:44:00 +0300

    @criss-vevo

--- plugins/ping.js
+++ plugins/ping.js
@@ -41,8 +41,8 @@ async (conn, mek, m, { from, quoted, sender, reply, }) => {
                 forwardingScore: 999,
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
-                    newsletterJid: '120363378608564635@newsletter',
-                    newsletterName: "CRISS AI SUPPORT",
+                    newsletterJid: '120363417599637828@newsletter',
+                    newsletterName: "CRISS AI",
                     serverMessageId: 143
                 }
             }
