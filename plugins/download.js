83e5e74a58de04abef12dcd0df2be09505c135fc
criss-vevo <crissvevo@icloud.com>
2025-05-24 21:16:31 +0300

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -305,7 +305,7 @@ cmd({
         document: { url: downloadUrl },
         mimetype: response.data.result.mimetype,
         fileName: response.data.result.fileName,
-        caption: "*© Powered By JawadTechX*"
+        caption: "*© Powered By CrissVevo*"
       }, { quoted: m });
 
       await conn.sendMessage(from, { react: { text: "✅", key: m.key } });
