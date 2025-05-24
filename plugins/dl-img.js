e1a0fcf7fb920a069bb65c5ac462aedfdd0daec3
criss-vevo <crissvevo@icloud.com>
2025-05-24 21:50:18 +0300

    @criss-vevo

--- plugins/dl-img.js
+++ plugins/dl-img.js
@@ -37,7 +37,7 @@ cmd({
                 from,
                 { 
                     image: { url: imageUrl },
-                    caption: `📷 Result for: ${query}\n> © Powered by CRISS AI`
+                    caption: `📷 Result for: ${query}\n> © Powered by CrissVevo`
                 },
                 { quoted: mek }
             );
