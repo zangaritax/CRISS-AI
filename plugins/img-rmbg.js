8d64c9cb1d5e864ab33e29a08563a94cf0e02f50
criss-vevo <crissvevo@icloud.com>
2025-05-24 21:24:26 +0300

    Update img-rmbg.js

--- plugins/img-rmbg.js
+++ plugins/img-rmbg.js
@@ -75,7 +75,7 @@ cmd({
 
     await conn.sendMessage(m.chat, {
       image: imageBuffer,
-      caption: `Background removed\n\n> *Powered by JawadTechX*`
+      caption: `Background removed\n\n> *Powered by CrissVevo*`
     });
 
   } catch (error) {
