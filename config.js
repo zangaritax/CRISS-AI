d3f07f4c039d62d3a491eea5f59e3e3666dcdb53
criss-vevo <crissvevo@icloud.com>
2025-06-30 20:02:07 +0300

    @criss-vevo

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
 // make true to know who dismiss or promoted a member in group
 ANTI_LINK: process.env.ANTI_LINK || "false",
 // make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "true",
+MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/j67u2n.jpg",
 // add custom menu and mention reply image url
